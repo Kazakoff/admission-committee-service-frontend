492629e8af708092c2f23e0f7851141eeeb3d6d6
Roman Boikov <romkaboikov@gmail.com>
2018-03-24 04:44:20 +0300

    added changes in component

--- src/app/PersonalInformation/personal.info.component.ts
+++ src/app/PersonalInformation/personal.info.component.ts
@@ -1,18 +1,26 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Personal} from './personal';
 import {HttpService} from './personal.service';
 import {Docseria} from './docseria';
 import {Doctype} from './doctype';
 import {Nationality} from './nationality';
+import {timeout} from 'q';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'personal',
   templateUrl: './personal.info.component.html',
-  providers: [HttpService]
+  styles: [`
+        input.ng-touched.ng-invalid {border:solid red 2px;}
+        input.ng-touched.ng-valid {border:solid green 2px;}
+    `],
+  providers: [HttpService, DatePipe]
 })
 export class PersonalInfoComponent implements OnInit {
+  @ViewChild('personaldocumentOrgan') el: ElementRef;
 
   personal: Personal = new Personal();
+  personalObject: Personal[] = [];
   sex = true;
 
   receivedPersonal: Personal; // полученный пользователь
@@ -20,8 +28,22 @@ export class PersonalInfoComponent implements OnInit {
   doctype: Doctype[] = [];
   docseria: Docseria[] = [];
   nationality: Nationality[] = [];
+  personalEdited: Personal;
+  documenttypeEdited: Doctype;
+  documentseriaEdited: Docseria;
+  documentnumberEdited: string;
+  documentdateEdited: Date;
+  docorganEdited: string;
+  firstnameEdited: string;
+  lastnameEdited: string;
+  middlenameEdited: string;
+  birthdateEdited: Date;
+  birthplaceEdited: string;
+  nationalityEdited: Nationality;
+  identificationnumberEdited: string;
+  condition = true;
 
-  constructor(private httpService: HttpService) {
+  constructor(private httpService: HttpService, public datepipe: DatePipe) {
   }
 
   submit(personal: Personal) {
@@ -41,6 +63,37 @@ export class PersonalInfoComponent implements OnInit {
 
 
   ngOnInit() {
+    this.httpService.getAbitur().subscribe(data => {
+      this.personalObject = data['profileInfo'];
+      localStorage.setItem('personal', JSON.stringify(this.personalObject));
+    });
+
+    this.personalEdited = JSON.parse(localStorage.getItem('personal'));
+    this.docorganEdited = this.personalEdited['documentOrgan'];
+    this.personal.documentOrgan = this.docorganEdited;
+    this.documenttypeEdited = this.personalEdited['documentType'];
+    this.personal.documentTypeId = this.documenttypeEdited;
+    this.documentnumberEdited = this.personalEdited['documentNumber'];
+    this.personal.documentNumber = this.documentnumberEdited;
+    //this.el.nativeElement.target = this.personal.documentTypeId.name;
+    this.documentdateEdited = <Date>this.datepipe.transform(this.personalEdited['documentDate'], 'yyyy-MM-dd');
+    //const documentdate = new Date(this.personalEdited['documentDate']);
+    //this.documentdateEdited = <Date>documentdate.toISOString().split('T')[0];
+    this.personal.documentDate = this.documentdateEdited;
+    this.firstnameEdited = this.personalEdited['firstName'];
+    this.personal.firstName = this.firstnameEdited;
+    this.lastnameEdited = this.personalEdited['lastName'];
+    this.personal.lastName = this.lastnameEdited;
+    this.middlenameEdited = this.personalEdited['middleName'];
+    this.personal.middleName = this.middlenameEdited;
+    const birthdate = new Date(this.personalEdited['birthDate']);
+    this.birthdateEdited = <Date>birthdate.toISOString().split('T')[0];
+    this.personal.birthDate = this.birthdateEdited;
+    this.birthplaceEdited = this.personalEdited['birthPlace'];
+    this.personal.birthPlace = this.birthplaceEdited;
+    this.identificationnumberEdited = this.personalEdited['identificationNumber'];
+    this.personal.identificationNumber = this.identificationnumberEdited;
+
     this.httpService.getAbitur().subscribe(data => this.httpService.userid = data['id']);
     this.httpService.getDocSeria().subscribe(data => this.docseria = data['content']);
     this.httpService.getDocType().subscribe(data => this.doctype = data['content']);
