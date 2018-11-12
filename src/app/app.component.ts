cc9e5f2ac84db0143648fecac4f2cf1d955de551
Roman Boikov <romkaboikov@gmail.com>
2018-11-12 21:36:41 +0300

    status added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
-import {GET_ABITURIENT} from './URLS';
+import {GET_ABITURIENT, GET_STATUS} from './URLS';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
@@ -10,6 +10,9 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   approve: boolean;
+  userId: number;
+  specialities: Object;
+  sum: number;
 
   constructor(public router: Router, private http: HttpClient) {}
 
@@ -28,10 +31,27 @@ export class AppComponent implements OnInit {
     return myHeaders;
   }
 
+  isStatusPass(type) {
+    return type === 'PASS';
+  }
+
+  isStatusNotPass(type) {
+    return type === 'NOT_PASS';
+  }
+
+  isStatusPriorityPass(type) {
+    return type === 'PRIORITY_PASS';
+  }
+
   ngOnInit() {
     if (this.token) {
       this.http.get(GET_ABITURIENT, {headers: this.addHeaders(), withCredentials: true}).subscribe(data => {
         this.approve = data['profileApproved'];
+        this.userId = data['id'];
+        this.http.get(GET_STATUS + this.userId + '/status', {headers: this.addHeaders(), withCredentials: true}).subscribe(response => {
+          this.specialities = response['specialities'];
+          this.sum = response['sum'];
+        });
       });
     }
   }
