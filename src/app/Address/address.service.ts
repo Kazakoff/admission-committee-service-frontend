1886d5dcd9603874bf94dfc0df6ed4dca8e41440
Kazakoff <C:\Users\User\AppData\Roaming\The Bat!>
2020-04-30 15:50:16 +0300

    comment

--- src/app/Address/address.service.ts
+++ src/app/Address/address.service.ts
@@ -1,27 +1,30 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Address} from './address';
-import {GET_ABITURIENT, PUT_ABITURIENT_ADDRESS, GET_CITY_FRAGMENT_ADDRESS, POST_CITY, GET_REGION_FRAGMENT} from '../URLS';
-import {NewCity} from './newCity';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Address } from "./address";
+import {
+  GET_ABITURIENT,
+  PUT_ABITURIENT_ADDRESS,
+  GET_CITY_FRAGMENT_ADDRESS,
+  POST_CITY,
+  GET_REGION_FRAGMENT,
+} from "../URLS";
+import { NewCity } from "./newCity";
 
 @Injectable()
 export class HttpService {
-
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   userid: number;
-  public token = JSON.parse(localStorage.getItem('token'));
+  public token = JSON.parse(localStorage.getItem("token"));
 
   addHeaders() {
     const myHeaders = new HttpHeaders()
-      .set('Authorization', 'Bearer ' + this.token)
-      .set('Content-Type', 'application/json');
+      .set("Authorization", "Bearer " + this.token)
+      .set("Content-Type", "application/json");
     return myHeaders;
   }
-
+  // добавить поля
   postData(address: Address) {
-
     const body = {
       postCode: address.postCode,
       street: address.street,
@@ -36,12 +39,14 @@ export class HttpService {
 
     return this.http.put(PUT_ABITURIENT_ADDRESS + this.userid, body, {
       headers: this.addHeaders(),
-      withCredentials: true
+      withCredentials: true,
     });
   }
 
   getAbitur() {
-    return this.http.get(GET_ABITURIENT, {headers: this.addHeaders(), withCredentials: true});
+    return this.http.get(GET_ABITURIENT, {
+      headers: this.addHeaders(),
+      withCredentials: true,
+    });
   }
-
 }
