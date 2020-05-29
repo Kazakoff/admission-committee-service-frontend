e3a80c08d06a8269de7bd677b983254582334e02
Kazakoff <C:\Users\User\AppData\Roaming\The Bat!>
2020-05-29 16:20:01 +0300

    After Auth location ref change for production

--- src/app/Authorization/auth.component.ts
+++ src/app/Authorization/auth.component.ts
@@ -1,59 +1,68 @@
-import {Component, OnInit} from '@angular/core';
-import {Auth} from './auth';
-import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { Auth } from "./auth";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { environment } from "../../environments/environment";
 
-
-import { NotifierService } from 'angular-notifier';
-import {GENERATE_TOKEN} from '../URLS';
+import { NotifierService } from "angular-notifier";
+import { GENERATE_TOKEN } from "../URLS";
 
 @Component({
-  selector: 'auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css'],
+  selector: "auth",
+  templateUrl: "./auth.component.html",
+  styleUrls: ["./auth.component.css"],
 })
 export class AuthComponent implements OnInit {
-  constructor(private http: HttpClient, private _service: NotifierService) {
-  }
-auth: Auth = new Auth();
-error: number;
-token: any;
+  constructor(private http: HttpClient, private _service: NotifierService) {}
+  auth: Auth = new Auth();
+  error: number;
+  token: any;
 
   addHeaders() {
     return new HttpHeaders()
-      .set('Authorization', 'Basic QURNSVNTSU9OX0NMSUVOVDpoMnUkTVRONVcm')
-      .set('Content-Type', 'application/json');
+      .set("Authorization", "Basic QURNSVNTSU9OX0NMSUVOVDpoMnUkTVRONVcm")
+      .set("Content-Type", "application/json");
   }
 
   addParams() {
     return new HttpParams()
-      .set('grant_type', 'password')
-      .set('username', this.auth.email)
-      .set('password', this.auth.password);
+      .set("grant_type", "password")
+      .set("username", this.auth.email)
+      .set("password", this.auth.password);
   }
 
   successEvent() {
-    this._service.notify('success', 'Добро пожаловать в приёмную комиссию');
+    this._service.notify("success", "Добро пожаловать в приёмную комиссию");
   }
 
   errorEvent() {
-    this._service.notify('error', 'Ошибка авторизации!');
+    this._service.notify("error", "Ошибка авторизации!");
   }
 
   submit() {
-    this.http.post(GENERATE_TOKEN, {}, {headers: this.addHeaders(), withCredentials: true, params: this.addParams()})
+    this.http
+      .post(
+        GENERATE_TOKEN,
+        {},
+        {
+          headers: this.addHeaders(),
+          withCredentials: true,
+          params: this.addParams(),
+        }
+      )
       .subscribe(
         (data) => {
-          this.token = data['access_token'];
-          localStorage.setItem('token', JSON.stringify(this.token));
+          this.token = data["access_token"];
+          localStorage.setItem("token", JSON.stringify(this.token));
           this.error = undefined;
           this.successEvent();
-          location.replace('/');
+          location.replace(environment.authRedirectURL);
         },
-        error => { this.error = error; this.errorEvent(); }
+        (error) => {
+          this.error = error;
+          this.errorEvent();
+        }
       );
   }
 
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
 }
